a8850007a47779cc2bdb57af5f001cfabee0bd80
System Administrator <root@Pace-Wisdoms-MacBook-Air.local>
2021-01-19 15:47:50 +0530

    home page added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HomeService } from '../services/home.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
 
+  cardCountsData:any;
+
+  constructor(
+    private _service: HomeService
+  ) { }
+
+
+  ngOnInit() {
+    this.getCardsCount();
+  }
+
+
+  getCardsCount(): void {
+    this._service.getCardCounts().subscribe(res => {
+      this.cardCountsData =res;
+    }, err => {
+      console.log(err);
+    })
+  }
 }
